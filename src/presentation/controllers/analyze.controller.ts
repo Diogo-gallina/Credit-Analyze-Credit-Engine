881dc2131247796a7819b15891d4cceb5028b9ca
diogo_gallina <diogogallina20@gmail.com>
2024-07-25 17:35:07 -0300

    feat: create analyze controller

--- src/presentation/controllers/analyze.controller.ts
+++ src/presentation/controllers/analyze.controller.ts
@@ -1,5 +1,32 @@
-import { Controller } from '@presentation/protocols';
+import { AnalysisResultUseCase } from '@app/use-cases/analisisResult/analysisResult';
+import { SendAnalysisResultToQueueUseCase } from '@app/use-cases/send-analysis-result-to-queue/SendAnalysisResultToQueue';
+import { DbAddAnalysisResult } from '@data/use-cases/add-analisis-result/dbAddAnalysisResult';
+import { AddAnalysisResultModel } from '@domain/use-cases/addAnalysisResult';
+import { AnalyzeDataDto } from '@presentation/dtos/analyzeDataDto';
+import { badRequest, ok } from '@presentation/helper/httpHelper';
+import { Controller, HttpRequest, HttpResponse } from '@presentation/protocols';
 
-export class AnalizeController implements Controller {
-  async handle(request): Promise<any> {}
+export class AnalyzeController implements Controller {
+  constructor(
+    private readonly analysisResultUseCase: AnalysisResultUseCase,
+    private readonly dbAddAnalysisResult: DbAddAnalysisResult,
+    private readonly sendAnalysisResultToQueueUseCase: SendAnalysisResultToQueueUseCase,
+  ) {
+    this.analysisResultUseCase = analysisResultUseCase;
+    this.dbAddAnalysisResult = dbAddAnalysisResult;
+    this.sendAnalysisResultToQueueUseCase = sendAnalysisResultToQueueUseCase;
+  }
+
+  async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+    try {
+      const analysisReultRequestBody: AnalyzeDataDto = httpRequest.body;
+      const { invoice, user } = analysisReultRequestBody;
+      const analysisResultData: AddAnalysisResultModel = await this.analysisResultUseCase.execute(invoice, user);
+      const analysisResult = await this.dbAddAnalysisResult.add(analysisResultData);
+      await this.sendAnalysisResultToQueueUseCase.execute(analysisResult);
+      return ok('Send result successfully');
+    } catch (error) {
+      return badRequest(error);
+    }
+  }
 }
