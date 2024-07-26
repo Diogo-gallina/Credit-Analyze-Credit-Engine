f468914b5be8f6d929ca5adf79e11dd66bfe479f
diogo_gallina <diogogallina20@gmail.com>
2024-07-26 18:04:13 -0300

    refactor: change file and path name

--- src/infra/db/analysis-result-repository/analysisResult.ts
+++ src/infra/db/analysis-result-repository/analysisResult.ts
@@ -3,10 +3,10 @@ import { AnalysisResultModel } from '@domain/models/analysisResult';
 import { AddAnalysisResultModel } from '@domain/use-cases/addAnalysisResult';
 import { MongoHelper } from '../helpers/mongoHelper';
 
-export class AnalisisResultMongoRepository implements AddAnalysisResultRepository {
-  async add(analisisResult: AddAnalysisResultModel): Promise<AnalysisResultModel> {
+export class AnalysisResultMongoRepository implements AddAnalysisResultRepository {
+  async add(analysisResult: AddAnalysisResultModel): Promise<AnalysisResultModel> {
     const analysisResultCollection = await MongoHelper.getCollection('analysisResults');
-    const result = await analysisResultCollection.insertOne(analisisResult);
+    const result = await analysisResultCollection.insertOne(analysisResult);
     const { insertedId } = result;
     return MongoHelper.map(await analysisResultCollection.findOne({ _id: insertedId }));
   }
