9103e6d6c1c3709f4f83cd872be40b362dbe7fdd
diogo_gallina <diogogallina20@gmail.com>
2024-07-27 17:39:28 -0300

    chore: configure cron jobs

--- src/app/server.ts
+++ src/app/server.ts
@@ -1,9 +1,23 @@
+import dotenv from 'dotenv';
 import { MongoHelper } from '@infra/db/helpers/mongoHelper';
+import { schedule } from 'node-cron';
+import { MessagingAdapter } from '@infra/cloud/adapters/messaging/messagingAdapter';
+import { sqsHelper } from '@infra/cloud/aws/helpers/messaging/sqsHelper';
 import env from './config/env';
+import { ConsumesAnalyzeDataUseCase } from './use-cases/consumes-analyze-data/consumesAnalyzeData';
+
+dotenv.config();
 
 MongoHelper.connect(env.mongoUrl)
   .then(async () => {
     const app = (await import('./config/app')).default;
     app.listen(env.port, () => console.log(`Server running at http://localhost:${env.port}`));
+
+    const messagingAdapter = new MessagingAdapter(sqsHelper);
+    const consumesAnalyzeDataUseCase = new ConsumesAnalyzeDataUseCase(messagingAdapter);
+
+    schedule('*/5 * * * * *', async () => {
+      await consumesAnalyzeDataUseCase.execute();
+    });
   })
   .catch(console.error);
