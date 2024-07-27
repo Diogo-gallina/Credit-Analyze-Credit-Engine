cfc13eb1d02af43e3a41fbaa28544f85782d0ea4
diogo_gallina <diogogallina20@gmail.com>
2024-07-27 17:33:40 -0300

    feat: create sqs helper

---
+++ src/infra/cloud/aws/helpers/messaging/sqsHelper.ts
@@ -0,0 +1,71 @@
+import {
+  DeleteMessageCommand,
+  ReceiveMessageCommand,
+  SendMessageCommand,
+  SendMessageCommandInput,
+  SQSClient,
+} from '@aws-sdk/client-sqs';
+import { FailedSendMessageError } from '../../errors/FailedSendMessageError';
+import { IMessagingHelper } from '../../protocols/messagingHelperInterface';
+import { awsConfig } from '../../config/awsConfig';
+
+const client = new SQSClient(awsConfig);
+
+export const sqsHelper: IMessagingHelper = {
+  async sendMessageToQueue<T>(message: T, queueName: string, messageGroupId: string): Promise<void> {
+    console.log('SEND MESSAGE AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA: ', client.config.region);
+    const queueUrl = `https://sqs.${awsConfig.region}.amazonaws.com/339712871292/${queueName}`;
+    const params: SendMessageCommandInput = {
+      QueueUrl: queueUrl,
+      MessageBody: JSON.stringify(message),
+      MessageGroupId: messageGroupId,
+      MessageDeduplicationId: `message-${Date.now()}`,
+    };
+
+    const command = new SendMessageCommand(params);
+
+    try {
+      await client.send(command);
+      console.log('Message sent to SQS queue:', queueName);
+    } catch (error) {
+      throw new FailedSendMessageError(`Error sending message to SQS queue: ${error}`);
+    }
+  },
+
+  async consumesMessage<T>(queueName: string): Promise<T[]> {
+    const queueUrl = `https://sqs.${awsConfig.region}.amazonaws.com/339712871292/${queueName}`;
+    const params = {
+      QueueUrl: queueUrl,
+      MaxNumberOfMessages: 10,
+      VisibilityTimeout: 15,
+      WaitTimeSeconds: 10,
+    };
+
+    try {
+      const receiveCommand = new ReceiveMessageCommand(params);
+      const { Messages } = await client.send(receiveCommand);
+
+      console.log('MENSAGENS', { Messages });
+      if (!Messages || Messages.length === 0) {
+        console.log('No messages to process.');
+        return [];
+      }
+      const processedMessages: T[] = Messages.map((message) => {
+        const parsedMessage: T = JSON.parse(message.Body!);
+        const deleteParams = {
+          QueueUrl: queueUrl,
+          ReceiptHandle: message.ReceiptHandle!,
+        };
+        console.log('INICIANDO DELEÇÃO DA MENSAGEM');
+        const deleteCommand = new DeleteMessageCommand(deleteParams);
+        client.send(deleteCommand);
+        return parsedMessage;
+      });
+
+      return processedMessages;
+    } catch (error) {
+      console.error('Error receiving or deleting messages:', error);
+      return [];
+    }
+  },
+};
