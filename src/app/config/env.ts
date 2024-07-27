7a37475ecfc5422152a168564d50a74998040eaa
diogo_gallina <diogogallina20@gmail.com>
2024-07-27 17:27:24 -0300

    refactr: add fixed mongo URL

--- src/app/config/env.ts
+++ src/app/config/env.ts
@@ -3,6 +3,6 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export default {
-  mongoUrl: process.env.MONGO_URL,
-  port: process.env.PORT || 8080,
+  mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/credit-engine',
+  port: process.env.PORT || 8081,
 };
